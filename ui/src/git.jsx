b1c476781e201a2705bcc35bcd2987eb56b87ca7
monishkumar <monish1421@gmail.com>
2025-04-30 23:02:09 +0530

    ui

--- ui/src/git.jsx
+++ ui/src/git.jsx
@@ -2,75 +2,95 @@ import React, { useEffect, useRef, useState } from 'react';
 import cytoscape from "cytoscape";
 import dagre from 'cytoscape-dagre'
 import axios from 'axios'
+import './styles.css';
 
-const Git = () => {
-    const gitref = useRef(null);
-
-    const[gitInstance, setGitInstane] = useState(null);
-
-    const[newCommitResponse, setNewCommitResponse] = useState({});
-    const[prevCommit, setPrevCommit] = useState('init');
-
-    const[commitName, setCommitName] = useState('');
-    const[commitMessage, setCommitMessage] = useState('');
-
-    useEffect(() => {
-
-        // cytoscape.use(dagre);
-        // const cy = cytoscape({
-        //     container : gitref.current,
-        //     elements : [
-        //         {data : {id : 'init', message : 'Initialized Git Repo'}},
-        //     ],
-        //     style : [
-        //         {
-        //             selector: "node",
-        //             style: {
-        //               "background-color": "#0074D9",
-        //               label: "data(id)",
-        //             },
-        //         },
-        //         {
-        //             selector: "edge",
-        //             style: {
-        //                 "line-color": "#ccc",
-        //                 "target-arrow-color": "#ccc",
-        //                 "target-arrow-shape": "triangle",
-        //                 "curve-style": "bezier",
-        //             },
-        //         },
-        //     ],
-        //     layout : {
-        //         name : 'dagre',
-        //         directed : 'true',
-        //         padding : 10
-        //     }
-        // })
-
-        // setGitInstane(cy);
-
-        // cy.on('tap', 'node', (event) => {
-        //     const nodeId = event.target.data('message'); 
-        //     alert(`Node clicked: ${nodeId}`); 
-        // });
-
-        // return () => cy.destroy()
-    }, [])
-
-    const handleNewCommit = async() => {
-            // await axios.post("http://localhost:8080/newcommit", {
-            //         "name" : commitName,
-            //         "message" : commitMessage
-            // })
-            // .then((res) => {
-            //     if(!res) return;
-            //     setPrevCommit(newCommitResponse.commitId || prevCommit)
-            //     setNewCommitResponse(res.data);
-                
-            // })
-    }
-
-    useEffect(() => {
+const GitRepository = () => {
+  const gitref = useRef(null);
+  
+  const [gitInstance, setGitInstance] = useState(null);
+  const [newCommitResponse, setNewCommitResponse] = useState({});
+  const [prevCommit, setPrevCommit] = useState('init');
+  
+  const [branchName1, setBranchName1] = useState('');
+  const [branchName2, setBranchName2] = useState('');
+  const [mergeBranch, setMergeBranch] = useState('');
+  const [commitMessage, setCommitMessage] = useState('');
+  
+  useEffect(() => {
+  
+          cytoscape.use(dagre);
+          const cy = cytoscape({
+              container : gitref.current,
+              elements : [
+                  {data : {id : 'init', message : 'Initialized Git Repo'}},
+              ],
+              style : [
+                  {
+                      selector: "node",
+                      style: {
+                        "background-color": "#0074D9",
+                        label: "data(id)",
+                      },
+                  },
+                  {
+                      selector: "edge",
+                      style: {
+                          "line-color": "#ccc",
+                          "target-arrow-color": "#ccc",
+                          "target-arrow-shape": "triangle",
+                          "curve-style": "bezier",
+                      },
+                  },
+              ],
+              layout : {
+                  name : 'dagre',
+                  directed : 'true',
+                  padding : 10
+              }
+          })
+  
+          setGitInstance(cy);
+  
+          cy.on('tap', 'node', (event) => {
+              const nodeId = event.target.data('message'); 
+              alert(`Node clicked: ${nodeId}`); 
+          });
+  
+          return () => cy.destroy()
+      }, [])
+  
+  const handleNewBranch = async () => {
+    // Implementation for creating a new branch
+    console.log(`Creating new branch: ${branchName1}`);
+    console.log(`Merging branch: ${branchName2}`);
+    // API call would go here
+  };
+  
+  const handlePrint = () => {
+    console.log('Printing repository visualization');
+    window.print();
+  };
+  
+  const handleMerge = async () => {
+    // Implementation for merging branches
+    console.log(`Merging branch: ${mergeBranch}`);
+    // API call would go here
+  };
+  
+  const handleNewCommit = async() => {
+              // await axios.post("http://localhost:8080/newcommit", {
+              //         "name" : commitName,
+              //         "message" : commitMessage
+              // })
+              // .then((res) => {
+              //     if(!res) return;
+              //     setPrevCommit(newCommitResponse.commitId || prevCommit)
+              //     setNewCommitResponse(res.data);
+                  
+              // })
+      }
+  
+      useEffect(() => {
     
         // if (gitInstance && newCommitResponse.commitId) {
         //     const elements = [];
@@ -99,22 +119,81 @@ const Git = () => {
         //     gitInstance.layout({ name: 'dagre' }).run();
         // }
     }, [newCommitResponse]);
-    
+      
+  return (
+    <div className="git-container">
+      <div className="git-sidebar">
+        <div className="git-title">GIT BRANCH REPOSITORY</div>
+        
+        <div className="button-row">
+          <input 
+            type="text" 
+            className="git-input"
+            placeholder="Enter branch name" 
+            value={branchName1} 
+            onChange={(e) => setBranchName1(e.target.value)}
+          />
+          <button 
+            className="git-button branch-button"
+            onClick={handleNewBranch}
+          >
+            Branch
+          </button>
+        </div>
+        
+        <div className="button-row">
+          <input 
+            type="text" 
+            className="git-input"
+            placeholder="Enter branch 1 to merge" 
+            value={branchName1} 
+            onChange={(e) => setBranchName1(e.target.value)}
+          />
+          <input 
+            type="text" 
+            className="git-input"
+            placeholder="Enter branch 2 to merge" 
+            value={branchName2} 
+            onChange={(e) => setBranchName2(e.target.value)}
+          />
+          <button 
+            className="git-button merge-button"
+            onClick={handleMerge}
+          >
+            Merge
+          </button>
+        </div>
+        
+        <div className="button-row">
+          <div className="git-label">Commit</div>
+          <textarea 
+            className="git-textarea"
+            placeholder="Enter commit message" 
+            value={commitMessage} 
+            onChange={(e) => setCommitMessage(e.target.value)}
+          />
+          <button 
+            className="git-button commit-button"
+            onClick={handleNewCommit}
+          >
+            Create Commit
+          </button>
+        </div>
+        
+        <div className="button-row">
+          <button 
+            className="git-button print-button"
+            onClick={handlePrint}
+          >
+            Print Repository
+          </button>
+        </div>
+      </div>
+      
+      <div ref={gitref} style={{ width: '1000px', height: '1000px', border: '1px solid #ccc' }} />
 
-    return (
-        <>
-            <label htmlFor="name">Enter Commit Name : </label>
-            <input type="text" id='name' value={commitName} onChange={(e) => setCommitName(e.target.value)}  />
+    </div>
+  );
+};
 
-            <label htmlFor="message">Enter Commit Message : </label>
-            <input type="text" id='message'  value={commitMessage} onChange={(e) => setCommitMessage(e.target.value)} />
-
-            <button
-                onClick={() => handleNewCommit()}
-            >New commit</button>
-            <div ref={gitref} style={{ width: '1000px', height: '1000px', border: '1px solid #ccc' }} />
-        </>
-    );
-}
-
-export default Git;
+export default GitRepository;
